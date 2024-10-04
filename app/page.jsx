3822810a452142ae221c7a0056dbf93091e407e2
hassanr01 <hassanrageh.236@gmail.com>
2024-10-04 18:56:12 +0300

    Home Page Structuring

---
+++ app/page.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import StarsCanvas from "./components/main/StarBackground";
+
+export default function Home() {
+  return (
+    <>
+      <StarsCanvas />
+      <div className="title my-10">
+        <h1 className="text-white w-full text-center text-4xl font-bold">Rock Simulator</h1>
+      </div>
+      {/* Solar System Canvas */}
+
+
+
+
+
+
+      {/* Analysis for Objects */}
+
+
+
+
+
+      {/* what's our objectives */}
+
+
+
+
+      
+    </>
+  );
+}
